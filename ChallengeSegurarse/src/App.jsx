cfc21a295204802dadb4ccb735b75832cf2b3128
AgusRomero1404 <romeroagustin1404@gmail.com>
2024-04-09 21:32:34 -0300

    LayoutAdmin / NavBar

--- ChallengeSegurarse/src/App.jsx
+++ ChallengeSegurarse/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Menu from './components/Menu'
+import Menu from './components/NavMenu'
 import Home from './components/Home'
 import {Link, Route , Routes} from 'react-router-dom'
  
--- ChallengeSegurarse/src/main.jsx
+++ ChallengeSegurarse/src/main.jsx
@@ -4,9 +4,11 @@ import Login from './components/Login.jsx'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Admin from './components/Admin.jsx'
+import NavMenu from './components/NavMenu.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Login />
+    <Admin />
   </BrowserRouter>,
 )
